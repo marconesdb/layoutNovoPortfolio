e30de8e4302c4e59bde35a048f41aae548a7834f
marconebrito <marcone.brito@elystech.com.br>
2024-10-03 18:05:17 -0300

    Criação do componente Projects

--- src/components/Section/Projects/Projects.styles.ts
+++ src/components/Section/Projects/Projects.styles.ts
@@ -13,9 +13,7 @@ export const Container = styled.section`
   margin-top: 0;
   width: 100%;  /* Ocupar 100% da largura do Main */
   max-width: 100%; /* Limitar a largura máxima para prevenir overflow */
-  height: 100vh;
   box-sizing: border-box; /* Inclui padding no cálculo da largura */
-  overflow-x: hidden; /* Prevenir rolagem horizontal no container */
   background-color: ${primaryColor};
 `;
 
@@ -26,10 +24,185 @@ export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   width: 100%; /* Ocupa 100% da largura do container */
   max-width: 1200px; /* Limite de largura máxima */
   flex-wrap: wrap; /* Permitir quebra de linha se necessário */
   box-sizing: border-box; /* Previne overflow horizontal com padding */
 
+`;
+
+
+export const Title = styled.div`
+  
+  width: 100%; /* Garante que o título ocupe a largura total */
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start; /* Garante alinhamento do título e linha */
+
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    width: 80%;
+  }
+
+  span {
+    color: #A8990F; /* Cor amarela */
+  }
+  p{
+    font-size: 32px;
+  }
+
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    p {
+      font-size: 24px;
+    }
+  }
+`;
+
+export const Line = styled.div`
+  height: 1px;
+  width: 600px;
+  background-color: #A8990F;
+  margin-left: 10px;
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    
+  }
+
+
+`;
+
+export const Cards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin-top: 50px;
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr); /* Um cartão por linha */
+    gap: 15px; /* Reduz o espaço entre os cartões */
+  }
+`;
+
+export const Card = styled.div`
+  background-color: #1b1e23;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  color: white;
+  width: 300px;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border: solid 1px #ABB2BF;
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    width: 100%; /* Ajusta a largura para 100% em telas menores */
+    height: auto; /* Permite que a altura se ajuste dinamicamente */
+  }
+`;
+
+export const CardImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: contain;
+
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    height: 150px; /* Reduz a altura da imagem em dispositivos menores */
+  }
+`;
+
+export const CardTitle = styled.h3`
+  font-size: 24px;
+  font-weight: 500;
+  color: #fff;
+  text-align: center;
+  width: 300px;
+  padding: 10px;
+
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    width: 100%; /* Ajusta a largura para 100% */
+    font-size: 20px; /* Reduz o tamanho da fonte */
+  }
+`;
+
+export const CardSubtitle = styled.h3`
+  font-size: 14px;
+  font-weight: 400;
+  color: #ABB2BF;
+  text-align: center;
+  width: 300px;
+  padding: 10px;
+  border: solid 1px #ABB2BF;
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    width: 100%; /* Ajusta a largura para 100% */
+  }
+`;
+
+export const CardText = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  color: #ABB2BF;
+  text-align: center;
+  flex: 1;
+  margin-bottom: 15px;
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    font-size: 12px; /* Reduz o tamanho da fonte */
+  }
+`;
+
+export const CardButtons = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 15px;
+
+  a {
+    text-decoration: none;  /* Remove sublinhado do link */
+  }
+
+  button {
+    background-color: #1b1e23;
+    border: solid 1px #ABB2BF;
+    color: white;
+    padding: 10px 20px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 10px;  /* Espaço entre o ícone e o texto */
+    transition: background-color 0.3s;
+
+    &:hover {
+      /* background-color: #d9a400; */
+      background-color: #1b1e23;
+      color: white;
+      border: solid #d9a400;
+    }
+
+    svg {
+      font-size: 18px;  /* Tamanho do ícone */
+    }
+  }
+
+  /* Responsividade */
+  @media (min-width: 360px) and (max-width: 768px) {
+    flex-direction: column; /* Empilha os botões verticalmente */
+    gap: 10px; /* Reduz o espaçamento entre os botões */
+    justify-content: center; /* Centraliza os botões horizontalmente */
+    align-items: center; /* Centraliza os botões verticalmente */
+  }
 `;
--- src/components/Section/Projects/Projects.tsx
+++ src/components/Section/Projects/Projects.tsx
@@ -1,15 +1,124 @@
-
 import { 
   Container, 
   ContentWrapper, 
-   
-} from '../Projects/Projects.styles';
+  Title, 
+  Line, 
+  Cards, 
+  Card, 
+  CardImage, 
+  CardTitle, 
+  CardText, 
+  CardButtons, 
+  CardSubtitle, 
+} from './Projects.styles';
+
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+import Image1 from '../../../assets/images/img-projetos/Laços de Amor 1.png';
+import Image2 from '../../../assets/images/img-projetos/Image.png';
+import Image3 from '../../../assets/images/img-projetos/Karine Cevalles 2.png';
+import Image4 from '../../../assets/images/img-projetos/Trabalhadores.png';
+import Image5 from '../../../assets/images/img-projetos/Landpage.png';
+import Image6 from '../../../assets/images/img-projetos/Calculadora.png';
+
+type Project = {
+  image: string;
+  subtitle: string;
+  title: string;
+  description: string;
+  githubLink: string;
+  externalLink: string;
+};
+
+const projects: Project[] = [
+  {
+    image: Image1,
+    subtitle: "HTML, Tailwind.CSS e Javascript",
+    title: "Laços de Amor",
+    description: "Este é um site feito voluntariamente para a Instituição Laços de Amor.",
+    githubLink: "https://github.com/marconesdb/site-lacosdeamor",
+    externalLink: "https://site-lacosdeamor.vercel.app/"
+  },
+  {
+    image: Image2,
+    subtitle: "HTML, Tailwind.CSS e React.JS",
+    title: "Solar de Jesus",
+    description: "Este é um site feito voluntariamente para a Instituição de Caridade Solar de Jesus.",
+    githubLink: "https://github.com/marconesdb/solardejesus-ecommerce",
+    externalLink: "https://solardejesus-ecommerce.vercel.app/"
+  },
+  {
+    image: Image3,
+    subtitle: "HTML, Tailwind.CSS e Javascript",
+    title: "Karina Cevalles",
+    description: "Protótipo de um site para a Advogada Karine Cevalles.",
+    githubLink: "https://github.com/marconesdb/site-karinaCevalles",
+    externalLink: "https://site-karina-cevalles.vercel.app/"
+  },
+  {
+    image: Image4,
+    subtitle: "HTML, Tailwind.CSS e Javascript",
+    title: "T. da Última Hora",
+    description: "Protótipo de um site para a Instituição de Caridade Trabalhadores da Última Hora.",
+    githubLink: "https://github.com/marconesdb/SiteTrabalhadoresDaUltimaHora",
+    externalLink: "https://marconesdb.github.io/SiteTrabalhadoresDaUltimaHora/"
+  },
+  {
+    image: Image5,
+    subtitle: "HTML e CSS",
+    title: "Tom's Jazz School",
+    description: "Exemplo de uma LandPage.",
+    githubLink: "https://github.com/marconesdb/landPageOneBitCode",
+    externalLink: "https://marconesdb.github.io/landPageOneBitCode/"
+  },
+  {
+    image: Image6,
+    subtitle: "HTML, CSS, Javascript",
+    title: "Cal. Científica",
+    description: "Exemplo de uma Calculadora Científica.",
+    githubLink: "https://github.com/marconesdb/CalculadoraCientifica",
+    externalLink: "https://marconesdb.github.io/CalculadoraCientifica/"
+  },
+];
+
+function ProjectCard({ image, subtitle, title, description, githubLink, externalLink }: Project) {
+  return (
+    <Card>
+      <CardImage src={image} alt={`Imagem do ${title}`} />
+      <CardSubtitle>{subtitle}</CardSubtitle>
+      <CardText>
+        <CardTitle>{title}</CardTitle>
+        {description}
+      </CardText>
+      <CardButtons>
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+          <button>
+            <FaGithub /> Github
+          </button>
+        </a>
+        <a href={externalLink} target="_blank" rel="noopener noreferrer">
+          <button>
+            <FaExternalLinkAlt /> Page
+          </button>
+        </a>
+      </CardButtons>
+    </Card>
+  );
+}
 
 const Projects = () => {
   return (
     <Container id="#">
       <ContentWrapper>
-       
+        <Title>
+          <span>#</span>
+          <p>Projetos</p>
+          <Line />
+        </Title>
+        <Cards>
+          {projects.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </Cards>
       </ContentWrapper>
     </Container>
   );
