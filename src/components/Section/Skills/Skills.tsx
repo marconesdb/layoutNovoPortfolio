af807ecc1d5133c6d251e9627f9da62e87e138fc
marconebrito <marcone.brito@elystech.com.br>
2024-10-20 23:33:11 -0300

    Tradução do componente Skills

--- src/components/Section/Skills/Skills.tsx
+++ src/components/Section/Skills/Skills.tsx
@@ -1,13 +1,11 @@
-
+import { useTranslation } from 'react-i18next'; // Importar o hook de tradução
 import { 
   Container, 
   ContentWrapper,
   IconeLogo,
   ImagensInferiorContainer,
   ImagensSuperiorContainer, 
-   
 } from '../Skills/Skills.styles';
-
 
 import {  
   Line, 
@@ -16,89 +14,85 @@ import {
   Title 
 } from './Skills.styles';
 
-
 const Skills = () => {
+  const { t } = useTranslation(); // Usar o hook de tradução
+
   return (
     <Container id="skills">
       <ContentWrapper>
-      <Title>
-        <span>#</span>
-        <p>Skills</p>
-        <Line />
-      </Title>
-      <MainContainer>
-    
-        <div>
-          <ImagensSuperiorContainer>
-            <div className='cubo1'>
-              {/* Criação do cubo */}
-              {[...Array(25)].map((_, index) => (
-                <div key={index} className="dot"></div>
-              ))}
-            </div>
-            <div className='quadrado1'></div>
-          </ImagensSuperiorContainer>
-          <ImagensInferiorContainer>
-            <IconeLogo>
-              <div>
-                <div className='RetanguloInferior'>
-                  <div className='RetanguloInferiorMenor'></div>
+        <Title>
+          <span>#</span>
+          <p>{t('skills.title')}</p> {/* Traduzindo o título "Skills" */}
+          <Line />
+        </Title>
+        <MainContainer>
+          <div>
+            <ImagensSuperiorContainer>
+              <div className='cubo1'>
+                {[...Array(25)].map((_, index) => (
+                  <div key={index} className="dot"></div>
+                ))}
+              </div>
+              <div className='quadrado1'></div>
+            </ImagensSuperiorContainer>
+            <ImagensInferiorContainer>
+              <IconeLogo>
+                <div>
+                  <div className='RetanguloInferior'>
+                    <div className='RetanguloInferiorMenor'></div>
+                  </div>
+                  <div className='RetanguloSuperior'>
+                    <div className='RetanguloSuperiorMenor'></div>
+                  </div>
                 </div>
-                <div className='RetanguloSuperior'>
-                  <div className='RetanguloSuperiorMenor'></div>
+              </IconeLogo>
+              <div className='cubo2'>
+                {[...Array(25)].map((_, index) => (
+                  <div key={index} className="dot"></div>
+                ))}
+              </div>
+              <div className='quadrado2'></div>
+            </ImagensInferiorContainer>
+          </div>
+
+          <SkillsContainer>
+            <div className='Container'>
+              <div className="Container Content">
+                {/* Coluna 1 */}
+                <div className="coluna coluna1">
+                  <div className="Skills-1">
+                    <p className='p-1'>{t('skills.categories.languages')}</p> {/* Traduzindo "Linguagens" */}
+                    <p className='p-texto'>{t('skills.items.languages')}</p> {/* Traduzindo as linguagens */}
+                  </div>
+                </div>
+
+                {/* Coluna 2 */}
+                <div className="coluna coluna2">
+                  <div className="Skills-2">
+                    <p className='p-2'>{t('skills.categories.databases')}</p> {/* Traduzindo "Databases" */}
+                    <p className='p-texto'>{t('skills.items.databases')}</p> {/* Traduzindo os bancos de dados */}
+                  </div>
+                  <div className="Skills-3">
+                    <p className='p-3'>{t('skills.categories.others')}</p> {/* Traduzindo "Outros" */}
+                    <p className='p-texto'>{t('skills.items.others')}</p> {/* Traduzindo outros itens */}
+                  </div>
+                </div>
+
+                {/* Coluna 3 */}
+                <div className="coluna coluna3">
+                  <div className="Skills-4">
+                    <p className='p-4'>{t('skills.categories.tools')}</p> {/* Traduzindo "Ferramentas" */}
+                    <p className='p-texto'>{t('skills.items.tools')}</p> {/* Traduzindo ferramentas */}
+                  </div>
+                  <div className="Skills-5">
+                    <p className='p-5'>{t('skills.categories.frameworks')}</p> {/* Traduzindo "Frameworks" */}
+                    <p className='p-texto'>{t('skills.items.frameworks')}</p> {/* Traduzindo frameworks */}
+                  </div>
                 </div>
               </div>
-            </IconeLogo>
-            <div className='cubo2'>
-              {/* Criação do cubo */}
-              {[...Array(25)].map((_, index) => (
-                <div key={index} className="dot"></div>
-              ))}
             </div>
-            <div className='quadrado2'></div>
-          </ImagensInferiorContainer>
-        </div>
-      
-        <SkillsContainer>
-        <div className='Container'>
-
-      <div className="Container Content">
-        {/* <!-- Coluna 1 --> */}
-        <div className="coluna coluna1">
-            <div className="Skills-1">
-            <p className='p-1'>Linguagens</p>
-            <p className='p-texto'>TypeScript, JavaScript</p>
-            </div>
-        </div>
-
-        {/* <!-- Coluna 2 --> */}
-        <div className="coluna coluna2">
-            <div className="Skills-2">
-            <p className='p-2'>Databases</p>
-            <p className='p-texto'>MySQL, PostgreSQL</p>
-            </div>
-            <div className="Skills-3">
-            <p className='p-3'>Outros</p>
-            <p className='p-texto'>HTML, CSS, EJS, SCSS</p>
-            </div>
-        </div>
-
-        {/* <!-- Coluna 3 --> */}
-        <div className="coluna coluna3">
-            <div className="Skills-4">
-            <p className='p-4'>Ferramentas</p>
-            <p className='p-texto'>VSCode, Insomnia, Figma, Font Awesome</p>
-            </div>
-            <div className="Skills-5">
-            <p className='p-5'>Frameworks</p>
-            <p className='p-texto'>Tailwind.CSS, Bootstrap, React.JS</p>
-            </div>
-        </div>
-    </div>
-
-        </div>
-        </SkillsContainer>
-      </MainContainer>
+          </SkillsContainer>
+        </MainContainer>
       </ContentWrapper>
     </Container>
   );
