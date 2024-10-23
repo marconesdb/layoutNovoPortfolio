ae1fd73a11f142cb33468572861b9a4d138954bc
marconebrito <marcone.brito@elystech.com.br>
2024-10-22 23:24:39 -0300

    Validação do Formulário com API EmailJS

--- src/components/Section/Formulario/Formulario.tsx
+++ src/components/Section/Formulario/Formulario.tsx
@@ -6,9 +6,44 @@ import {
   Main 
 } from '../Formulario/Formulario.styles';
 import { useTranslation } from 'react-i18next'; // Importar o hook de tradução
+import {useState} from 'react';
+import emailjs from '@emailjs/browser'
 
 const Formulario = () => {
   const { t } = useTranslation(); // Usar o hook de tradução
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [assunto, setAssunto] = useState('');
+
+  function sendEmail(e) {
+    e.preventDefault();
+    if(name === '' || email === '' || message === ''){
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    const templateParams = {
+    from_name: name,
+    message: message,
+    email: email,
+    assunto: assunto
+    }
+
+
+  emailjs.send("service_fwsd2n6", "template_vhv4uql", templateParams, "jPQQSpaqYqSpawUEu")
+  .then((response) => {
+    console.log("EMAIL ENVIADO", response.status, response.text)
+    console.log("EMAIL ENVIADO", response.status, response.text)
+    setName('')
+    setEmail('')
+    setAssunto('')
+    setMessage('')
+  }, (err) => {
+    console.log("ERRO: ", err)
+  })
+
+}
 
   return (
     <Container id="#">
@@ -17,7 +52,7 @@ const Formulario = () => {
           <FormularioWrapper>
             <div className="container">
               <h2>{t('form.title')}</h2> {/* Tradução "Fale comigo!" */}
-              <form action="#" method="POST">
+              <form className="form" onSubmit={sendEmail}>
                 <div className="form-group">
                   <div className="form-item">
                     <label htmlFor="nome">{t('form.name.label')}</label> {/* Tradução "Nome:" */}
@@ -27,6 +62,8 @@ const Formulario = () => {
                       name="nome" 
                       required 
                       placeholder={t('form.name.placeholder')} // Tradução do placeholder "Digite seu nome..."
+                      onChange={(e) => setName(e.target.value)}
+                      value={name}
                     />
                   </div>
                   <div className="form-item">
@@ -37,6 +74,8 @@ const Formulario = () => {
                       name="email" 
                       required 
                       placeholder={t('form.email.placeholder')} // Tradução do placeholder "Digite seu email..."
+                      onChange={(e) => setEmail(e.target.value)}
+          value={email}
                     />
                   </div>
                 </div>
@@ -49,21 +88,25 @@ const Formulario = () => {
                     name="assunto" 
                     required 
                     placeholder={t('form.subject.placeholder')} // Tradução do placeholder "Digite um assunto..."
+                    onChange={(e) => setAssunto(e.target.value)}
+                    value={assunto}
                   />
                 </div>
 
                 <div className="form-group">
                   <label htmlFor="mensagem">{t('form.message.label')}</label> {/* Tradução "Mensagem:" */}
-                  <textarea 
+                  <textarea
                     id="mensagem" 
                     name="mensagem" 
                     rows={5} 
                     required 
                     placeholder={t('form.message.placeholder')} // Tradução do placeholder "Digite aqui sua mensagem..."
+                    onChange={(e) => setMessage(e.target.value)}
+                    value={message}
                   ></textarea>
                 </div>
 
-                <EnviarButton>{t('form.button')}</EnviarButton>  {/* Tradução do botão "Enviar" */}
+                <EnviarButton className="button" value="Enviar">{t('form.button')}</EnviarButton>  {/* Tradução do botão "Enviar" */}
               </form>
             </div>
           </FormularioWrapper>
